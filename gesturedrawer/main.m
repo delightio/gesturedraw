ab3d76469f85cdd2c0acbd8ed5b5372f0624f9dd
Bill So <hokchun@gmail.com>
2012-05-11 01:38:43 +0800

    Initial Commit

---
+++ gesturedrawer/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  gesturedrawer
+//
+//  Created by Bill So on 5/11/12.
+//  Copyright (c) 2012 Headnix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
