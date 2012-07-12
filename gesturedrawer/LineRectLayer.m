2fe321811b59a5fbebc729e39aa4010681fa84e1
billibala <hokchun@gmail.com>
2012-07-13 05:23:20 +0800

    Updated full path drawing [#249]

--- gesturedrawer/LineRectLayer.m
+++ gesturedrawer/LineRectLayer.m
@@ -38,11 +38,27 @@
 	CGPoint curPosition = self.position;
 	CGPoint abVec = CGPointMake(aPoint.x - curPosition.x, aPoint.y - curPosition.y);
 	double rotationAng = 0.0;
+	// check the quadrant to set rotation angle
 	if ( abVec.x == 0.0 ) {
-		rotationAng = abVec.y > 0.0 ? M_PI_2 : 0.0;
+		rotationAng = abVec.y > 0.0 ? -M_PI_2 : M_PI_2;
+	} else if ( abVec.x > 0.0 && abVec.y > 0.0 ) {
+		// first quadrant
+		rotationAng = atan(abVec.y / abVec.x);
+	} else if ( abVec.x < 0.0 && abVec.y > 0.0 ) {
+		// 2nd quadrant
+		rotationAng = (M_PI + atan(abVec.y / abVec.x));
+	} else if ( abVec.x < 0.0 && abVec.y < 0.0 ) {
+		// 3rd quadrant
+		rotationAng = (M_PI - atan(abVec.y / abVec.x));
 	} else {
+		// 4th quadrant
 		rotationAng = atan(abVec.y / abVec.x);
 	}
+//	if ( abVec.x == 0.0 ) {
+//		rotationAng = abVec.y > 0.0 ? M_PI_2 : 0.0;
+//	} else {
+//		rotationAng = atan(abVec.y / abVec.x);
+//	}
 	self.transform = CATransform3DMakeRotation(rotationAng, 0.0, 0.0, 1.0);
 	theBounds.size.width = sqrt( abVec.x * abVec.x + abVec.y * abVec.y );
 	return theBounds;
